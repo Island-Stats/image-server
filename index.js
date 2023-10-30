f57d76af79d0d9066c9eb7b7a22d564334d5c4a1
TheMysterys <the-mystery@outlook.com>
2023-10-30 16:15:21 +1300

    init commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.disable('x-powered-by')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+	res.redirect("https://islandstats.xyz/")
+})
+
+app.use((req, res, next) => {
+	res.status(404).end();
+})
+
+app.use((err, req, res, next) => {
+	res.sendStatus(500);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
