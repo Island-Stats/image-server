a5b857b82df90606b02e70477e4352f0e4f17be4
TheMysterys <the-mystery@outlook.com>
2023-10-30 21:39:14 +1300

    Added cache header

--- index.js
+++ index.js
@@ -4,17 +4,28 @@ const port = process.env.PORT || 3000
 
 app.disable('x-powered-by')
 
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-	res.redirect("https://islandstats.xyz/")
+// Cache images for 1 day
+app.use(function (req, res, next) {
+	res.set('Cache-Control', 'public, max-age=86400');
+	next();
 })
 
+// Serve images
+app.use(express.static('public'))
+
+// Redirect to main site
+app.get('/', (req, res) => {
+	res.redirect(301, "https://islandstats.xyz/")
+})
+
+// Not found
 app.use((req, res, next) => {
 	res.status(404).end();
 })
 
+// Error
 app.use((err, req, res, next) => {
+	console.error(err.stack);
 	res.sendStatus(500);
 })
 
