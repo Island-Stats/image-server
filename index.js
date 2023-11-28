329c176a2132b492a52e8a54cb901934f52fa9cc
TheMysterys <the-mystery@outlook.com>
2023-11-29 01:31:53 +1300

    Add HTML and CSS files for Island Stats CDN Docs

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.disable('x-powered-by')
+app.disable("x-powered-by");
 
 // Cache images for 1 day
 app.use(function (req, res, next) {
-	res.set('Cache-Control', 'public, max-age=86400');
+	res.set("Cache-Control", "public, max-age=86400");
 	next();
-})
+});
 
 // Serve images
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-// Redirect to main site
-app.get('/', (req, res) => {
-	res.redirect(301, "https://islandstats.xyz/")
-})
+// Send docs
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 // Not found
 app.use((req, res, next) => {
 	res.status(404).end();
-})
+});
 
 // Error
 app.use((err, req, res, next) => {
 	console.error(err.stack);
 	res.sendStatus(500);
-})
+});
 
 app.listen(port, () => {
-  console.log(`Image Server listening on port ${port}`)
-})
+	console.log(`Image Server listening on port ${port}`);
+});
